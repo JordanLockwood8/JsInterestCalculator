28671267dd957afc1daf82c08d47dd6f1ca31bb2
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-09-21 12:52:05 -0600

    interest calculator

---
+++ src/index.js
@@ -0,0 +1,34 @@
+class Interest{
+
+    simpleInterest(principle, time, rate)
+    {
+        console.log("Principle : " + principle);
+        console.log("Time : " + time);
+        console.log("Rate : " + rate);
+    
+        var amount = (principle * time * rate) / 100;
+    
+        console.log("Simple Interest : $" + amount + "\n");
+    }
+    compoundInterest(principle, time, rate, n){
+        console.log("Principle : " + principle);
+        console.log("Time : " + time);
+        console.log("Rate : " + rate);
+        //n= how many times interest is compounded per incrament of time
+        console.log("compound per year : " + n);
+
+        const amount = principle * (Math.pow((1 + (rate / n)), (n * time)));
+        const interest = amount - principle;
+        console.log ("Interest : $" + interest + "\n");
+        
+    }
+}
+let interest = new Interest();
+
+interest.simpleInterest(1000, 5, 1.5);
+
+interest.compoundInterest(1000, 5, 1.5, 12);
+
+
+
+    
