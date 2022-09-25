b80dd24969516270aa57d6aed28cec9866b0d71f
Dak <zawulf18@gmail.com>
2022-09-24 21:13:28 -0600

    Updated Interest to return results

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-const Interest = require("./interest")
+import {Interest} from "./interest.js";
 
 let interest = new Interest();
 
-// interest.simpleInterest(1000, 5, 1.5)
 const a = interest.simpleInterest(5, 5, 1)
 console.log(a)
 
 const b = interest.compoundInterest(10, 20, 30, 5)
 console.log(b)
-
-// interest.compoundInterest(1000, 5, 1.5, 12);
--- src/interest.js
+++ src/interest.js
@@ -1,29 +1,18 @@
-export class Interest{
+export class Interest {
 
-    simpleInterest(principle, time, rate)
-    {
-        // console.log("Principle : " + principle);
-        // console.log("Time : " + time);
-        // console.log("Rate : " + rate);
-    
+    simpleInterest(principle, time, rate) {
+        
         var amount = (principle * time * rate) / 100;
-    
-        // console.log("Simple Interest : $" + amount + "\n");
-
+        
         return amount;
     }
-    compoundInterest(principle, time, rate, n){
-        // console.log("Principle : " + principle);
-        // console.log("Time : " + time);
-        // console.log("Rate : " + rate);
-        //n= how many times interest is compounded per incrament of time
-        // console.log("compound per year : " + n);
-
+    
+    compoundInterest(principle, time, rate, n) {
+        
         const amount = principle * (Math.pow((1 + (rate / n)), (n * time)));
         const interest = amount - principle;
-        // console.log ("Interest : $" + interest + "\n");
-        return interest;
-
         
+        return interest;
     }
+
 }
