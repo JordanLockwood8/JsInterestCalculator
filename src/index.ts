2960d3e457c22904b5c3c91c49e250a46d30c536
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-10-16 12:47:16 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import {Interest} from "./interest.js";
+
+let interest = new Interest();
+
+const a = interest.simpleInterest(5, 5, 1)
+console.log(a)
+
+const b = interest.compoundInterest(10, 20, 30, 5)
+console.log(b)
+
+const c = 123;
